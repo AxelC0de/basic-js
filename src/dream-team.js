a5415d7df21ae2ae594210d357c411c17fee8f05
AxelC0de <AxelC0de.github@gmail.com>
2021-03-29 22:45:15 +0300

    feat: solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+	if (!Array.isArray(arr)) {
+		return false
+	}
+	let arrStr = []
+	for (const i of arr) {
+		if (typeof i === 'string') {
+			arrStr.push(i)
+		}
+	}
+
+	let arrFirst = []
+	for (let i of arrStr) {
+		i = i.trim().substring(0, 1).toUpperCase()
+		arrFirst.push(i)
+	}
+
+	// arrFirst = [... new Set(arrFirst)]
+
+	arrFirst.sort((a, b) => a.localeCompare(b));
+	arrFirst = arrFirst.join('')
+
+	return arrFirst;
 };
